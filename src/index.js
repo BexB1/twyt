8ac4c340f56eab8819a57987382c851d7863eb31
Bex B <bexb1@hotmail.com>
2020-05-15 13:09:57 +0100

    Feature: Redux store passed into App.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 import "./index.css";
 import App from "./components/App";
+import reducer from "./reducers";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(reducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
--- src/reducers/authedUser.js
+++ src/reducers/authedUser.js
@@ -1,4 +1,4 @@
-import { SET_AUTHED_USER } from "../actions/authedUser";
+import { SET_AUTHED_USER } from "../actions/authUser";
 
 export default function authedUser(state = null, action) {
   switch (action.type) {
